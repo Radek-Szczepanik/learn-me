7e4fa3db5219bfd8abfe94d441dae7581355e82e
Radek-Szczepanik <radekszczepanik.rsz@gmail.com>
2020-09-02 12:04:59 +0200

    Create AuthController

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LearnMeAPI.Data;
+using LearnMeAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LearnMeAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthRepository _repository;
+
+        public AuthController(IAuthRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(string username, string firstName, string lastName,
+                                                  string address, int postcode, string city, int telephone,
+                                                  string email, int nip, int pesel, string password)
+        {
+            username = username.ToLower();
+
+            if (await _repository.UserExist(username))
+                return BadRequest("Użytkownik o takiej nazwie już istnieje !");
+
+            var newUser = new User
+            {
+                Username = username,
+                FirstName = firstName,
+                LastName = lastName,
+                Address = address,
+                Postcode = postcode,
+                City = city,
+                Telephone = telephone,
+                Email = email,
+                NIP = nip,
+                PESEL = pesel
+            };
+
+            var createUser = await _repository.Register(newUser, password);
+
+            return StatusCode(201);
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -22,6 +22,7 @@ namespace LearnMeAPI
         {
             services.AddControllers();
             services.AddDbContext<DataContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddScoped<IAuthRepository, AuthRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
