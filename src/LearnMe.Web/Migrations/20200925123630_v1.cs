04ef1d7325e058f91cd977ed6f1a28821c73c2f4
lukaszkedziora <ad.rem1@gmail.com>
2020-09-25 14:42:27 +0200

    Initial migration Added

---
+++ src/LearnMe.Web/Migrations/20200925123630_v1.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LearnMe.Web.Migrations
+{
+    public partial class v1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
