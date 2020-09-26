d21d1f8ca5756fd03caa6d05eadbad2504f1b513
lukaszkedziora <ad.rem1@gmail.com>
2020-09-26 23:53:20 +0200

    Login service class and interface added

---
+++ src/LearnMe.Core/Services/Account/LoginService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LearnMe.Core.DTO.User;
+using LearnMe.Core.Interfaces;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using LearnMe.Shared.Enum;
+
+namespace LearnMe.Core.Services.Account
+{
+    class LoginService : ILoginService
+    {
+        [ValidateAntiForgeryToken]
+        public UserBasicDto RegisterHashDateTime(UserBasicDto user)
+        {
+            var temp = user.Password.GetHashCode();
+            user.Password = temp.ToString();
+            user.RegistrationDate = DateTime.UtcNow;
+            user.Status = UserStatus.Inactive;
+            return user;
+        }
+           
+    }
+}
