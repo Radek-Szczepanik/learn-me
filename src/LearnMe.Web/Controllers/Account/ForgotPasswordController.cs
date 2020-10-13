a7019011bbdaa60242d0d1a2bd4065da5ada6607
lukaszkedziora <ad.rem1@gmail.com>
2020-10-13 15:45:50 +0200

    Forget password update

--- src/LearnMe.Web/Controllers/Account/ForgotPasswordController.cs
+++ src/LearnMe.Web/Controllers/Account/ForgotPasswordController.cs
@@ -29,31 +29,31 @@ namespace LearnMe.Web.Controllers.Account
 
         //public async Task<IActionResult> OnPostAsync(LoginDto Input)
         //{
-        //        var user = await _userManager.FindByEmailAsync(Input.Email);
-        //        if (user == null || !(await _userManager.IsEmailConfirmedAsync(user)))
-        //        {
-        //            return RedirectToPage("./ForgotPasswordConfirmation");
-        //        }
+        //    var user = await _userManager.FindByEmailAsync(Input.Email);
+        //    if (user == null || !(await _userManager.IsEmailConfirmedAsync(user)))
+        //    {
+        //        return RedirectToPage("./ForgotPasswordConfirmation");
+        //    }
 
-            //    var token = await _userManager.GeneratePasswordResetTokenAsync(user);
-            //var confirmationLink = Url.Action("Register", "api", new { token, email = user.Email }, Request.Scheme);
-            //var message = new Message(new string[] { user.Email }, "Confirmation email link", confirmationLink, null);
-            //await _emailSender.SendEmailAsync(message);
+        //    var token = await _userManager.GeneratePasswordResetTokenAsync(user);
+        //    var confirmationLink = Url.Action("Register", "api", new { token, email = user.Email }, Request.Scheme);
+        //    var message = new Message(new string[] { user.Email }, "Confirmation email link", confirmationLink, null);
+        //    await _emailSender.SendEmailAsync(message);
 
-            //code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
-            //    var callbackUrl = Url.Page(
-            //        "/Account/ResetPassword",
-            //        pageHandler: null,
-            //        values: new { area = "Identity", code },
-            //        protocol: Request.Scheme);
+        //    code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+        //    var callbackUrl = Url.Page(
+        //        "/Account/ResetPassword",
+        //        pageHandler: null,
+        //        values: new { area = "Identity", code },
+        //        protocol: Request.Scheme);
 
-            //    await _emailSender.SendEmailAsync(
-            //        Input.Email,
-            //        "Reset Password",
-            //        $"Please reset your password by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+        //    await _emailSender.SendEmailAsync(
+        //        Input.Email,
+        //        "Reset Password",
+        //        $"Please reset your password by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
 
-              //  return RedirectToPage("./ForgotPasswordConfirmation");
-                      
-      //  }
+        //    return RedirectToPage("./ForgotPasswordConfirmation");
+
+        //}
     }
 }
