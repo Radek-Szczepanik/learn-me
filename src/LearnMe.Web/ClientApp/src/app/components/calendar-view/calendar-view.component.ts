9f3ab64011eed646d8cbabb61b88c70f75d843ab
AAndrzejczuk <ag2rio@gmail.com>
2020-10-14 20:01:00 +0200

    calendar display refactored

--- src/LearnMe.Web/ClientApp/src/app/components/calendar-view/calendar-view.component.ts
+++ src/LearnMe.Web/ClientApp/src/app/components/calendar-view/calendar-view.component.ts
@@ -1,14 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { CalendarService } from '../../services/calendar/calendar-service'
-import { DxSchedulerModule } from 'devextreme-angular';
-import { CalendarEvent } from '../../services/calendar/calendar-event';
-
-import DataSource from 'devextreme/data/data_source';
-import CustomStore from 'devextreme/data/custom_store';
-import { HttpClient } from '@angular/common/http';
-import * as AspNetData from "devextreme-aspnet-data-nojquery";
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-calendar-view',
@@ -19,12 +10,8 @@ export class CalendarViewComponent implements OnInit {
 
   appointmentsData: any;
   currentDate: Date = new Date(2020, 9, 14);
-  //url = "https://localhost:44359/api/CalendarEvents?eventsPerPage=2&pageNumber=1";
 
   constructor(private data: CalendarService) {
-
-    //this.appointmentsData = [{ title: "Codecool meeting - postponed to Friday", description: "Learn-me project sprint planning", startDate: "2020-10-16T06:00:00.671", endDate: "2020-10-16T08:00:00.671", isDone: false, calendarId: "oo913fskjsio7n3krp6lvhg1r4" }];
-
     console.debug('appointmentsData:');
     console.debug(this.appointmentsData);
   }
