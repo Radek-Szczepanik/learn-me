b3ade136d3209331115d43e6b3c8724a2ba34b6d
AAndrzejczuk <ag2rio@gmail.com>
2021-02-13 22:09:19 +0100

    student upload of homeworks frontend + preliminary backend

--- src/LearnMe.Web/ClientApp/src/app/components/lesson/lesson-actions/lesson-actions.component.ts
+++ src/LearnMe.Web/ClientApp/src/app/components/lesson/lesson-actions/lesson-actions.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { HttpClient, HttpEventType } from '@angular/common/http';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { LessonAppointmentTableEntry } from '../../../services/calendar/calendar-service-ver-2';
 import { HttpService } from '../../../services/http.service';
 
@@ -13,8 +14,12 @@ export class LessonActionsComponent implements OnInit {
   lesson: LessonAppointmentTableEntry;
 
   loggedUser: string[] = ['Student'];
+  message: string;
+  fileStream: string;
+  @Output() public onUploadFinished = new EventEmitter();
+  progress: number;
 
-  constructor(private https: HttpService) { }
+  constructor(private https: HttpService, private http: HttpClient) { }
 
   ngOnInit(): void {
     const routeGetLoggedUser: string  = '/api/Identity';
@@ -29,4 +34,25 @@ export class LessonActionsComponent implements OnInit {
     });
   }
 
+  public uploadFile = (files, lessonCalendarId) => {
+    if (files.length === 0) {
+      return;
+    }
+    let fileToUpload = <File>files[0];
+    const formData = new FormData();
+    formData.append('file', fileToUpload, fileToUpload.name);
+    formData.append('lessonCalendarId', lessonCalendarId);
+
+    // let postRoute = 'api/homework'; //lessonCalendarId?lessonCalendarId=' + lessonCalendarId;
+    this.http.post('api/homework', formData, {reportProgress: true, observe: 'events'})
+      .subscribe(event => {
+        if (event.type === HttpEventType.UploadProgress)
+          this.progress = Math.round(100 * event.loaded / event.total);
+        else if (event.type === HttpEventType.Response) {
+          this.message = 'Upload success.';
+          this.onUploadFinished.emit(event.body);
+          this.fileStream = fileToUpload.name;
+        }
+      });
+  }
 }
