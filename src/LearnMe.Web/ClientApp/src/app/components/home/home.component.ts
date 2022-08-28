0414bd12191869eb31a5d5ef8bb201c1549e2045
lukaszkedziora <kontakt@lukaszkedziora.com>
2022-08-28 11:36:36 +0200

    FrontEnd: home page responsive updated

--- src/LearnMe.Web/ClientApp/src/app/components/home/home.component.ts
+++ src/LearnMe.Web/ClientApp/src/app/components/home/home.component.ts
@@ -31,12 +31,21 @@ export class HomeComponent  {
   sliderList: Slider[] = new Array();
   slider: Slider;
   breakpoint: number;
+  colspanVal: number;
+  rowspanSecond: number;
+  rowspanLogo: number;
+  rowspanAbout: number;
+
+  temp: boolean;
+  temp2: boolean;
+
   opinions = [];
   
   constructor(config: NgbCarouselConfig, http: HttpClient, @Inject('BASE_URL') baseUrl: string, public dialog: MatDialog) {
     config.interval = 10000;
     config.keyboard = true;
     config.pauseOnHover = true;
+    config.showNavigationArrows = false;
     this._http = http;
     this._baseUrl = baseUrl;
   }
@@ -46,6 +55,14 @@ export class HomeComponent  {
     this._http.get<Opinion[]>(this._baseUrl + 'api/Opinions').subscribe(result => {
       this.getSlider(result) ;
       });
+      
+      this.getAbout();
+
+      this.colspanVal = (window.innerWidth <= 1200) ? 10 : 5
+      this.rowspanSecond = (window.innerWidth <= 1200) ? 10 : 5
+      this.temp = (window.innerWidth <= 1200) ? false : true
+      this.temp2 = (window.innerWidth <= 1200) ? true : false
+      this.rowspanLogo = (window.innerWidth <= 1200) ? 7 : 5 
     }
     
     public getSlider(opinions: any) {
@@ -63,13 +80,41 @@ export class HomeComponent  {
   
         this.sliderList.push(this.slider);
       });
-      this.breakpoint = (window.innerWidth <= 800) ? 6 : 12
+    }
 
+    public getAbout() {
+      if (window.screen.width <= 300)  this.rowspanAbout=28;
+      else if (window.innerWidth <= 400)  this.rowspanAbout=20;
+      else if (window.innerWidth <= 500)  this.rowspanAbout=16;
+      else if (window.innerWidth <= 600)  this.rowspanAbout=12;
+      else if (window.innerWidth <= 700)  this.rowspanAbout=10;
+      else if (window.innerWidth <= 800)  this.rowspanAbout=9;
+      else if (window.innerWidth <= 900)  this.rowspanAbout=8;
+      else if (window.innerWidth >= 1000)  this.rowspanAbout=5;
     }
   
-  // onResize(event) {
-  //   this.breakpoint = (event.target.innerWidth <= 400) ? 1 : 6;
-  //}
+  onResize(event) {
+    this.getAbout();
+
+    if (event.target.innerWidth <= 1200)
+    {
+      this.colspanVal = 10
+      this.rowspanSecond = 10
+      this.temp = false 
+      this.temp2 = true
+      this.rowspanLogo = 7
+
+    } else {
+      this.colspanVal =  5
+      this.rowspanSecond = 5
+      this.temp = true
+      this.temp2 = false
+      this.rowspanLogo = 5
+    }
+  }
+
+
+  
   
 }
 
